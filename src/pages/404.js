f301cc05af2a7464023f61ca9ed90e86b7f541d6
Stephen Chiang <stephen.e.chiang@gmail.com>
2019-07-14 12:14:31 +0200

    DEV: Added styled-components plugin

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import css from "../css/error.module.css"
+import Banner from "../components/Banner"
 
 const errorPage = () => {
+  const title = "Oops! It's a dead end!"
+  const info = ""
   return (
     <Layout>
-      <article>
-        <h1>Error page</h1>
+      <article className={css.error}>
+        <Banner title={title} info={info}>
+          <Link className="btn-white" to="/">
+            Back to homepage
+          </Link>
+        </Banner>
       </article>
     </Layout>
   )
